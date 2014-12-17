742ae9537fa48c3ead9b3cc1943c6f81c735b378
Charlie Bevis <charliebevis@users.noreply.github.com>
2014-12-17 14:33:09 -0600

    move dictionary parsing into seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-# require_relative '../lib/cmudict_parser'
-# CmudictParser.parse
-
+require_relative '../lib/cmudict_parser'
 
 PhonemeType.find_or_create_by phoneme_type: 'affricate'
 PhonemeType.find_or_create_by phoneme_type: 'aspirate'
@@ -17,3 +15,26 @@ PhonemeType.find_or_create_by phoneme_type: 'nasal'
 PhonemeType.find_or_create_by phoneme_type: 'semivowel'
 PhonemeType.find_or_create_by phoneme_type: 'stop'
 PhonemeType.find_or_create_by phoneme_type: 'vowel'
+
+class Escaper
+  def self.escape(str)
+    ret_str = []
+    str.each_char do |c|
+      if c == "'"
+        ret_str << "''"
+      else
+        ret_str << c
+      end
+    end
+    ret_str.join
+  end
+end
+
+words = CmudictParser.parse('/Users/charliebevis/workspace/rails_rhymebook/lib/cmudict/cmudict.0.7a')
+values = []
+words.each do |word|
+  values << "('#{Escaper.escape(word[:spelling])}','#{word[:pronunciation]}', #{word[:syllables]})"
+end
+connection = ActiveRecord::Base.connection
+connection.execute "insert into Words (spelling,pronunciation,syllables) values #{values.join(',')}"
+
